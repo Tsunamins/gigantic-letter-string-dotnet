8b55b11ff69b9f29cf32117a9466b4e2f2567a3d
Alison Reilly <reillyamr@gmail.com>
2020-10-28 17:18:26 -0700

    add code to download file

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@ namespace ringba_test
     {
         static void Main(string[] args)
         {
-            
-
+            var wc = new System.Net.WebClient();
+            string remoteUri = "http://ringba-test-html.s3-website-us-west-1.amazonaws.com/TestQuestions/";
+            string fileName = "output.txt", myStringWebResource = null;
+            myStringWebResource = remoteUri + fileName;
+            Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, myStringWebResource);
+            wc.DownloadFile(myStringWebResource,fileName);		
+            Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, myStringWebResource);
+            Console.WriteLine("\nDownloaded file saved in the following file system folder:\n\t" + "Application.StartupPath");
 
         }
     }
